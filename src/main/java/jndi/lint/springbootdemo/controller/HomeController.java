592f21b057b74d400e7d448507931cce2bf11f5a
lint <lint@heerit.com>
2019-12-24 16:10:15 +0800

    first commit

---
+++ src/main/java/jndi/lint/springbootdemo/controller/HomeController.java
@@ -0,0 +1,12 @@
+package jndi.lint.springbootdemo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+	@GetMapping("")
+    public String hello() {
+        return "Hello!";
+    }
+}
